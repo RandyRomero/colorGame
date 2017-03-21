4eac54b117c1fa27e30ebc28b0488e7c97a2d758
RandyRomero <a.ya.miheev@gmail.com>
2017-03-21 15:29:03 +0300

    add missing semicolons and make variable to change color of header

--- js/main.js
+++ js/main.js
@@ -1,13 +1,14 @@
 console.log('Connected!')
 
-var restart = document.querySelector('#btn-1') /* new colors */
+var header = document.querySelector('header'); /* main header */
+var restart = document.querySelector('#btn-1'); /* new colors */
 var mode = document.querySelector('#btn-2'); /* easy or hard more */
 easyMode = true;
 
 /* define variables for squares */
 var sqrList =[];
 for(var i = 1; i <= 6; i++) {
-	sqrList[i] = document.querySelector('#sqr-' + i)
+	sqrList[i] = document.querySelector('#sqr-' + i);
 }
 
 /* change 'easy' button to 'hard' and back */
@@ -21,8 +22,10 @@ mode.addEventListener('click', function() {
 	}
 });
 
-restar.addEventListener('click', function() {
+/* returns default look to program */
+restart.addEventListener('click', function() {
+	header.style.backgroundColor = 'orange';
 	for(var i = 1; i <= 6; i++) {
 		sqrList[i].style.backgroundColor = 'orange';
 	}
-})  
+});  
