75784053c76a997a8258c032ede87ca5356ba13a
RandyRomero <a.ya.miheev@gmail.com>
2017-03-21 15:38:06 +0300

    make variable for user message and add it to restart function

--- js/main.js
+++ js/main.js
@@ -1,5 +1,6 @@
 console.log('Connected!')
 
+var userMessage = document.querySelector('main p')
 var header = document.querySelector('header'); /* main header */
 var restart = document.querySelector('#btn-1'); /* new colors */
 var mode = document.querySelector('#btn-2'); /* easy or hard more */
@@ -24,6 +25,7 @@ mode.addEventListener('click', function() {
 
 /* returns default look to program */
 restart.addEventListener('click', function() {
+	userMessage.style.display = 'none'; /* make it vanish */ 
 	header.style.backgroundColor = 'orange';
 	for(var i = 1; i <= 6; i++) {
 		sqrList[i].style.backgroundColor = 'orange';
