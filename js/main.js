299e327faf986aa2a611bc8e3779b96ed19aeb61
RandyRomero <a.ya.miheev@gmail.com>
2017-03-21 19:39:13 +0300

    fix loop that defines variables for squares in js; make function that paints all squares to random colors

--- js/main.js
+++ js/main.js
@@ -6,12 +6,8 @@ var restart = document.querySelector('#btn-1'); /* new colors */
 var mode = document.querySelector('#btn-2'); /* easy or hard more */
 var easyMode = true;
 
-/* get three random number from 0 to 255 */
-var rgbNums = [];
-for(var i = 0; i <= 2; i++) {
-	rgbNums[i] = Math.floor(Math.random() * 256);
-	console.log(rgbNums[i])
-}
+
+
 
 /* define variables for rgb numbers in header */
 var rgbNumHeader = [];
@@ -22,8 +18,9 @@ for(var i = 1; i <= 3; i++) {
 
 /* define variables for squares */
 var sqrList =[];
-for(var i = 1; i <= 6; i++) {
-	sqrList[i] = document.querySelector('#sqr-' + i);
+for(var i = 0; i <= 5; i++) {
+	sqrList[i] = document.querySelector('#sqr-' + (i + 1));
+	// console.log(sqrList.length
 }
 
 /* change 'easy' button to 'hard' and back */
@@ -41,8 +38,31 @@ mode.addEventListener('click', function() {
 restart.addEventListener('click', function() {
 	userMessage.style.display = 'none'; /* make it vanish */ 
 	header.style.backgroundColor = 'orange';
-	for(var i = 1; i <= 6; i++) {
-		sqrList[i].style.backgroundColor = 'orange';
-	}
+	paintSquares(sqrList);
 });
 
+
+/* get three random number from 0 to 255
+   return string for example: rgb(234, 54, 56) */
+function getRandomColor() {
+	var rgbNums = [];
+	for(var i = 0; i <= 2; i++) {
+		rgbNums[i] = Math.floor(Math.random() * 256);
+		// console.log(rgbNums[i])
+	};
+	var randomColor = 'rgb(' + rgbNums[0] + ', ' + rgbNums[1] + ', ' + rgbNums[2] + ')'
+	return randomColor
+}
+
+/* paints all squares in different colors */
+function paintSquares(sqrList) {
+	for(var i = 0; i < sqrList.length; i++) {
+		sqrList[i].style.backgroundColor = getRandomColor();
+	}
+}
+
+// paintSquares(sqrList);
+
+// for(var i = 0; i < sqrList.length; i++) {
+// 	sqrList[i].style.backgroundColor = 'orange';
+// }
